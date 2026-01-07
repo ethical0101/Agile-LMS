a783f4f9445ff4bd959bc4454891490418dcecec
ethical0101 <kommidruthendra2005@gmail.com>
2026-01-07 22:51:27 +0530

    Added Login Feature Code

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 def register(name):
     print(f"{name} has been registered.")
+
+#Login Feature code
+def login(user, password):
+    # simple hardcoded credentials
+    correct_user = "admin"
+    correct_password = "1234"
+
+    if user == correct_user and password == correct_password:
+        print("Login successful")
+    else:
+        print("Login failed")
+
+register("admin")
+login("admin", "1234")
