523a32e3229ec33b9551ce5b43526c0374f2512e
KOMMI-DRUTHENDRA <kommi.druthendra2023@vitstudent.ac.in>
2026-01-08 06:08:05 +0530

    pass Changed 5 char

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def register(name):
 #Login Feature code
 def login(user, password):
     correct_user = "admin"
-    correct_password = "1234"
+    correct_password = "12345"
 
     if user == correct_user and password == correct_password:
         print("Login successful")
