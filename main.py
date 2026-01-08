f5cc8f735279c64a2040f0c74b172ef03c4d37e3
KOMMI-DRUTHENDRA <kommi.druthendra2023@vitstudent.ac.in>
2026-01-08 06:02:56 +0530

    UserB improved login failure message

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ def register(name):
 
 #Login Feature code
 def login(user, password):
-    # simple hardcoded credentials
     correct_user = "admin"
     correct_password = "1234"
 
     if user == correct_user and password == correct_password:
         print("Login successful")
     else:
-        print("Login failed")
+        print("Invalid username or password")
+
 
 register("admin")
 login("admin", "1234")
